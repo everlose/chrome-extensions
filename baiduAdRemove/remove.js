1498eb78ca3f50cb7fd73cd6e07b6301bca39748
陈子云 <472206618@qq.com>
2016-09-17 19:52:00 +0800

    一直处于监听状态，一旦更新则就删除

--- baiduAdRemove/remove.js
+++ baiduAdRemove/remove.js
@@ -1,10 +1,14 @@
 'use strict';
 
-var baiduContainer = document.querySelector('#content_left');
-var normalContent = baiduContainer.querySelectorAll('.result-op, .result');
+var doRemove = function () {
+    var baiduContainer = document.querySelector('#content_left');
+    var normalContent = baiduContainer.querySelectorAll('.result-op, .result');
 
-var html = '';
-normalContent.forEach(function (v, k) {
-	html += v.outerHTML;
-});
-baiduContainer.innerHTML = html;
+    var html = '';
+    normalContent.forEach(function (v, k) {
+        html += v.outerHTML;
+    });
+    baiduContainer.innerHTML = html;
+};
+
+setInterval(doRemove, 500);
