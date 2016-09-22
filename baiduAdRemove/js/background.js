42ccf559a5f1cace1ac5508a204cab72c9a663ad
陈子云 <472206618@qq.com>
2016-09-22 10:45:57 +0800

    改为空格四格缩进

--- baiduAdRemove/js/background.js
+++ baiduAdRemove/js/background.js
@@ -4,15 +4,15 @@ var console = chrome.extension.getBackgroundPage().console;
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
-	console.log('tab.url=' + tab.url + " , changeInfo.status=" + changeInfo.status)
+    console.log('tab.url=' + tab.url + " , changeInfo.status=" + changeInfo.status)
 
-	if(tab.url != undefined && changeInfo.status == 'complete'){
+    if(tab.url != undefined && changeInfo.status == 'complete'){
 
-		// block all ads
-		if(tab.url.match('baidu.com')){
-			chrome.tabs.insertCSS(tabId, {file: "/css/baidu.css"});
-		} else if (tab.url.match('csdn.net')) {
-			chrome.tabs.insertCSS(tabId, {file: "/css/csdn.css"});
-		}
-	}
+        // block all ads
+        if(tab.url.match('baidu.com')){
+            chrome.tabs.insertCSS(tabId, {file: "/css/baidu.css"});
+        } else if (tab.url.match('csdn.net')) {
+            chrome.tabs.insertCSS(tabId, {file: "/css/csdn.css"});
+        }
+    }
 });
